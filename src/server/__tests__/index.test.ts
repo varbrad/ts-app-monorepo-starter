a680f4b8c537c8fecd0908b26b44b592eb9d2f50
Brad Davies <varbrad@users.noreply.github.com>
2019-06-15 16:53:58 +0100

    Get TSC working

--- src/server/__tests__/index.test.ts
+++ src/server/__tests__/index.test.ts
@@ -1,10 +1,6 @@
 import fn from '../index';
 
 describe('thing', (): void => {
-  it('does thing', (): void => {
-    expect(typeof document).toMatchInlineSnapshot(`"undefined"`);
-  });
-
   it('does another thing', (): void => {
     expect(fn(5)).toMatchInlineSnapshot(`3125`);
   });
